de2c08dbd440139a805d301faf29ba57c4943a9e
kissmy0926 <33798138+kissmy0926@users.noreply.github.com>
2021-03-28 23:47:21 +0800

    Add files via upload

---
+++ ID/src/com/company/ID.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class ID {
+	public static void main(String[] args) {
+		Boolean src = true;
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		while (src) {
+			System.out.println("1.隨機創建身分證");
+			System.out.println("2.身分證驗證");
+			System.out.println("輸入編號:");
+			String choose = sc.next();
+			switch (choose) {
+			case "1":
+				Id_from.creatId();
+				break;
+			case "2":
+				Id_from.checkID();
+				break;
+			}
+		}
+		/*
+		 * int x,y,z,w; Scanner sc = new Scanner(System.in); System.out.println("選擇選項");
+		 * x= sc.nextInt(); y=sc.nextInt(); z=sc.nextInt(); w=sc.nextInt();
+		 * System.out.println("1.隨機創建身分證"); System.out.println("2.身分證驗證");
+		 * System.out.println("3.EXIT");
+		 */
+	}
+}
